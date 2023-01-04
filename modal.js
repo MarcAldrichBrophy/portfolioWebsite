f3a32a99ebe19db26934aa4c0e34c1aae37cd71e
= <aldrichbrophy@gmail.com>
2023-01-04 13:31:25 -0800

    modal card

---
+++ modal.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.button').click(function(){
+        var buttonId = $(this).attr('id');
+        $('#modal-container').removeAttr('class').addClass(buttonId);
+        $('body').addClass('modal-active');
+    })
+
+    $('#modal-container').click(function(){
+        $(this).addClass('out');
+        $('body').removeClass('modal-active');
+    });
+
+    console.log("init modal.");
+
+});
